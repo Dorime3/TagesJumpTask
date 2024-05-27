2a4b8c97be71bf6abccca894227d73b392dfa404
r.denisov <r.denisov@basicdecor.ru>
2024-05-27 21:58:34 +0300

    fix_some_adaptive_styles

--- src/components/CatalogItem.vue
+++ src/components/CatalogItem.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, defineProps } from "vue";
-import LikeOutlineIcon from "../assets/icons/like-outline-icon.vue";
-import DoneCircleIcon from "../assets/icons/done-circle-icon.vue";
-import CartIcon from "../assets/icons/cart-icon.vue";
-import LikeFilledIcon from "../assets/icons/like-filled-icon.vue";
+import { computed, onMounted, ref, defineProps, toRefs } from "vue";
+import LikeOutlineIcon from "@/assets/icons/like-outline-icon.vue";
+import DoneCircleIcon from "@/assets/icons/done-circle-icon.vue";
+import CartIcon from "@/assets/icons/cart-icon.vue";
+import LikeFilledIcon from "@/assets/icons/like-filled-icon.vue";
 import { useStore } from "vuex";
 import { IItem } from "@/store/types";
 import { basePath } from "@/utils/config";
 
-interface Props {
-  item: IItem;
-}
+const props = defineProps<{ item: IItem }>();
 
-// eslint-disable-next-line vue/no-setup-props-destructure
-const { item } = defineProps<Props>();
+const { id, name, code, price, image } = toRefs(props.item);
 
 const isHoveredItem = ref(false);
 const isMobile = ref(false);
@@ -43,59 +40,48 @@ const favoriteItemsIds = computed(
     @mouseover="isHoveredItem = true"
     @mouseleave="isHoveredItem = false"
   >
-    <div
-      v-if="item && item.price && item.price.old_price"
-      class="main-section__card-sale"
-    >
-      Скидка
-    </div>
+    <div v-if="price?.old_price" class="main-section__card-sale">Скидка</div>
     <img
       class="main-section__card-img"
-      v-if="item && item.image"
-      :src="basePath + item?.image.url"
-      :alt="item?.name"
+      v-if="image"
+      :src="basePath + image.url"
+      :alt="name"
     />
     <div class="main-section__card-wrapper">
       <div class="main-section__card-info">
-        <p v-if="item?.code" class="main-section__card-info-subtext">
-          {{ item.code }}
+        <p v-if="code" class="main-section__card-info-subtext">
+          {{ code }}
         </p>
-        <h2 class="main-section__card-info-title">{{ item?.name }}</h2>
+        <h2 class="main-section__card-info-title">{{ name }}</h2>
         <div class="main-section__card-prices">
-          <div
-            v-if="item && item.price && item.price.old_price"
-            class="main-section__card-price-old"
-          >
-            {{ Math.round(item?.price.old_price || 0) }}₽
+          <div v-if="price?.old_price" class="main-section__card-price-old">
+            {{ Math.round(price.old_price || 0) }}₽
           </div>
-          <div
-            v-if="item && item.price && item.price.current_price"
-            class="main-section__card-price-new"
-          >
-            {{ Math.round(item?.price.current_price || 0) }}₽
+          <div v-if="price?.current_price" class="main-section__card-price-new">
+            {{ Math.round(price.current_price || 0) }}₽
           </div>
         </div>
       </div>
       <div class="main-section__card-actions">
         <DoneCircleIcon
-          v-if="cartItemsIds.includes(item?.id)"
-          @click="toggleCartItem(item?.id)"
+          v-if="cartItemsIds.includes(id)"
+          @click="toggleCartItem(id)"
           class="main-section__card-button remove-from-cart"
         />
         <CartIcon
           v-else
           v-show="isMobile || isHoveredItem"
-          @click="toggleCartItem(item?.id)"
+          @click="toggleCartItem(id)"
           class="main-section__card-button add-to-cart"
         />
         <LikeFilledIcon
-          v-if="favoriteItemsIds.includes(item?.id)"
-          @click="toggleFavoriteItem(item?.id)"
+          v-if="favoriteItemsIds.includes(id)"
+          @click="toggleFavoriteItem(id)"
           class="main-section__card-button unlike"
         />
         <LikeOutlineIcon
           v-else
-          @click="toggleFavoriteItem(item?.id)"
+          @click="toggleFavoriteItem(id)"
           class="main-section__card-button like"
         />
       </div>
