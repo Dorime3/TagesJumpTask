1c32e99d42d36b845d4f89a3b202bea11b132ca4
r.denisov <r.denisov@basicdecor.ru>
2024-05-27 19:04:09 +0300

    set_privacy_headers

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  devServer: {
+    headers: {
+      "Permissions-Policy": "",
+    },
+  },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/TagesJumpTask/" : "/",
 });
